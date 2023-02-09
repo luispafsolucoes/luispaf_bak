c7a0b8ab057eed6964c220904328fd91e6fe0a0c
luispafsolucoes <luispafsolucoes@gmail.com>
2023-02-09 20:25:11 -0300

    ajuste

--- src/main/java/com/projeto/luispaf/controller/VersionController.java
+++ src/main/java/com/projeto/luispaf/controller/VersionController.java
@@ -1,7 +1,5 @@
 package com.projeto.luispaf.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projeto.luispaf.model.Agenda;
-import com.projeto.luispaf.repository.AgendaRepository;
+import com.projeto.luispaf.repository.CaixaRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,29 +19,20 @@ import lombok.RequiredArgsConstructor;
 public class VersionController {
 	
 	@Autowired
-	AgendaRepository agendaRepository;
+	CaixaRepository caixaRepository;
 
 	@GetMapping("/status")
 	public ResponseEntity<?> getTotalPacoteDoDia() throws Exception{
 		try {
-			System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");			
-			return new ResponseEntity<>("Tudo OK", HttpStatus.OK);
+			System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+			Long qtde = caixaRepository.getQtdeCaixaCriadoDataAtual();
+			System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+			
+			return new ResponseEntity<>("QTDE: " + qtde, HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
 		}
 		
-	}
-	
-	@GetMapping("/agendadosDiasNteriores")
-	public ResponseEntity<?> getAgendadosDiasNteriores() throws Exception{
-		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-		List<Agenda> lista = agendaRepository.getClientesAgendadosDiasAnteriores();
-		
-		System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
-		System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
-		
-		return new ResponseEntity<>(lista, HttpStatus.OK);
 	}
 }
